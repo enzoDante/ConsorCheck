85082b92990017c17649192759e4d58127eb24b0
Enzo Dante <enzo.dante.m@gmail.com>
2026-08-27 20:57:14 -0300

    primeiro commit, iniciando projeto

---
+++ src/main/java/com/credito/ConsorCheck/ConsorCheckApplication.java
@@ -0,0 +1,13 @@
+package com.credito.ConsorCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsorCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsorCheckApplication.class, args);
+	}
+
+}
